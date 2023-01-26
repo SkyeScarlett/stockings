1ffca41ae5fb42791454556ea52dc46216e9e96e
Skye <anastasia_sorokina_90@mail.ru>
2023-01-26 22:51:06 +0300

    Help needed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
+    @api = StockQuote::Stock.new(api_key: 'pk_a853121062e34d0ca1a5c057a2f8165d')
+    Rails.logger.info(params[:ticker])
+    if params[:ticker] == ""
+      @nothing = "You have to enter a symbol"
+    elsif params[:ticker]
+   @stock = StockQuote::Stock.quote(params[:ticker])
+    end
+ end
+
+  def about
   end
 end
